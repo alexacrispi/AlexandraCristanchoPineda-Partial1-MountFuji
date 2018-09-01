36143577e0e3f4fd5091d34b8f0643d060838ee4
alexacrispi <alexa_cp23@hotmail.com>
2018-09-01 14:20:31 -0500

    Parcial

---
+++ AlexandraCristancho-MountFuji/Assets/SelfTranslation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfTranslation : MonoBehaviour {
+
+    public float rotationspeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        this.transform.Translate(0, 0, rotationspeed);
+		
+	}
+}
